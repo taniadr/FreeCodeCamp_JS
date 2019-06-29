e2fa37a1c7af1e3542d8e04bde979d017edbe7a0
Tania <taniadiasr@gmail.com>
2019-06-29 10:37:16 -0300

    Reduced code for recordCollection is ready

---
+++ recordCollection.js
@@ -0,0 +1,68 @@
+// Setup
+// JS Basics: Record Collection 
+// Code-reduced version
+// Old version available at "acessingNestedObjects.js"
+
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function addValueToProp(album, prop, value){
+    var aux = collection;
+    if (prop == "tracks")  
+        aux[album]["tracks"].push(value); 
+    else
+        aux[album][prop] = value;
+    return aux;
+}
+
+function updateRecords(id, prop, value) {
+    var temp = collection;
+    if (value == "") {
+        delete temp[id][prop];
+    }
+    else {
+        if (temp[id][prop]) {
+            temp = addValueToProp(id, prop, value);
+        }
+        else {
+            switch (prop) {
+                case "tracks":
+                    temp[id][prop] = [];
+                    temp[id]["tracks"].push(value);
+                    break;
+                default :
+                    temp[id][prop] = "";
+                    temp[id][prop] = value;
+            }       
+        }
+    }
+    collection = temp;
+    return collection;
+}
+
+updateRecords(5439, "artist", "ABBA");
+console.log(collection);
