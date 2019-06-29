4e18deb094ce635e97186bb944a4dc4682b88f43
Tania <taniadiasr@gmail.com>
2019-06-29 08:27:25 -0300

    Finally found out how to access this damn complex objects!!!

--- acessingNestedObjects.js
+++ acessingNestedObjects.js
@@ -1,48 +1,67 @@
 // Setup
+// JS Basics: Record Collection 
+// TanoviskY 29/06/2019 = TODAY WE FINISH THIS MODULE !!!
+
 var collection = {
-    "2548": {
-      "album": "Slippery When Wet",
-      "artist": "Bon Jovi",
-      "tracks": [ 
-        "Let It Rock", 
-        "You Give Love a Bad Name" 
-      ]
-    },
-    "2468": {
-      "album": "1999",
-      "artist": "Prince",
-      "tracks": [ 
-        "1999", 
-        "Little Red Corvette" 
-      ]
-    },
-    "1245": {
-      "artist": "Robert Palmer",
-      "tracks": [ ]
-    },
-    "5439": {
-      "album": "ABBA Gold"
-    }
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": [ 
+      "Let It Rock", 
+      "You Give Love a Bad Name" 
+    ]
+  },
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": [ 
+      "1999", 
+      "Little Red Corvette" 
+    ]
+  },
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": [ ]
+  },
+  "5439": {
+    "album": "ABBA Gold"
+  }
 };
+// Keep a copy of the collection for tests
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
+// Only change code below this line
 function updateRecords(id, prop, value) {
-
+  
+  var temp = collection;
+  
+  // ----> 1)se a prop não for tracks
   if (prop != "tracks"){
-    if (value)  
-        collection[id["prop"]] = value;
-    else 
-        collection[id["prop"]].pop();
-  }      
-  else {
-      if (collection[id["prop"]].length == 0) {
-        collection[id["prop"]] = [];   
-      }
-      collection[id["prop"]].push(value);
+    if (temp[id.prop])
+    {
+      temp[id.prop] = value;  
+    }
+    else {
+      temp[id][prop] = "";
+      temp[id][prop] = value;
+    }
+    
+  }
+  else if (prop == "tracks")
+  {
+    // ----> 2)se a prop for tracks mas for vazio
+    if (temp.id.length == 0)
+    {
+      temp.id.tracks = [];
+    }
+    temp.id.tracks.push(value);
+  }
+  collection = temp; 
+  
 
-      }
-  return collection;
+return collection;
 }
 
 // Alter values below to test your code
 updateRecords(5439, "artist", "ABBA");
+console.log(collection);
