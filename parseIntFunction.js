adf3612484d85f9a2a1a434413e2ee6702a482ae
Tania <taniadiasr@gmail.com>
2019-06-29 18:49:59 -0300

    ternary operations added

--- parseIntFunction.js
+++ parseIntFunction.js
@@ -1,5 +1,13 @@
 function convertToInteger(str) {
     return parseInt(str);
-  }
-  
-  convertToInteger("56");
+}
+
+convertToInteger("56");
+
+//parse with radix, to specify the numeric system of the number 
+
+function convertWithBase(str, radix) {
+    return parseInt(str, radix);
+}
+
+console.log(convertWithBase("11", 2)); 
---
+++ ternaryOperator.js
@@ -0,0 +1,9 @@
+function checkEqual(a, b) {
+    return (a == b) ? true : false;
+}
+console.log("checkEqual(a,b)" + checkEqual(1, 2));
+
+var n = 4;
+var m = 7;
+console.log("(n == m)? // (4 == 7)?\n");
+console.log ((n == m) ? true : false);
