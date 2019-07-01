d7460de4fd16ec2bcb582fa94d2c9c1575fe4880
Tania <taniadiasr@gmail.com>
2019-07-01 17:52:14 -0300

    Module ES6 of FreeCodeCamp started. In paralel, I am working at advanced stuff which is in the other directory , JS_ADV

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -7,3 +7,9 @@ var n = 4;
 var m = 7;
 console.log("(n == m)? // (4 == 7)?\n");
 console.log ((n == m) ? true : false);
+
+//you can use multiple ternary objects as well :
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+  }
